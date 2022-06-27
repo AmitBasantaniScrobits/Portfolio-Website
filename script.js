19f03047c5087ca652928119e63c784519678429
AmitBasantaniScrobits <107797123+AmitBasantaniScrobits@users.noreply.github.com>
2022-06-27 13:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ==================== typing animation ===================== */
+var typed = new Typed(".typing", {
+    strings:["","Computer Science Engineer","Web Designer", "Java Developer", "Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
